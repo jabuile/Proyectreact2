c35f5742a2433d7b539f07f5154f7da34b683db7
jabuile tovar <jabuiletovar@gmail.com>
2022-10-24 00:18:18 -0300

    agrgando redes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,9 @@ import NavBar from './NavBar'
 const Footer = () => {
   return (
     <footer className='Footer'>
-        Footer
+        <a href="https://www.instagram.com/"><img  src="/instagram_logo_2016.svg.png" alt="" /></a>
+        <a href="https://www.facebook.com/"><img  src="/logo face.jpg" alt="" /></a>
+        <a href="https://www.whatsapp.com/"><img  src="/logow.png" alt="" /></a>
         <NavBar/>
     </footer>
     
